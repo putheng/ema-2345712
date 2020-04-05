9557d35d5697e50463523d33bb70b6aa16d75d1a
vagrant <vagrant@homestead>
2020-04-05 14:21:43 +0000

    update product detail route

--- resources/views/home/partials/best-sale.blade.php
+++ resources/views/home/partials/best-sale.blade.php
@@ -1,109 +1,109 @@
 <section class="pt-5 pb-4">
-						<!-- Heading-->
-						<div class="d-flex flex-wrap justify-content-between align-items-center pt-1 border-bottom pb-4 mb-4">
-							<h2 class="h3 mb-0 pt-3 mr-3">Bestsellers</h2>
-							<div class="pt-3"><a class="btn btn-outline-accent btn-sm" href="grocery-catalog.html">More products<i class="czi-arrow-right ml-1 mr-n1"></i></a></div>
-						</div>
-						<div class="cz-carousel cz-controls-static cz-controls-outside cz-dots-enabled pt-2">
-							<div class="cz-carousel-inner" data-carousel-options="{&quot;items&quot;: 2, &quot;gutter&quot;: 16, &quot;controls&quot;: true, &quot;autoHeight&quot;: true, &quot;responsive&quot;: {&quot;0&quot;:{&quot;items&quot;:1}, &quot;480&quot;:{&quot;items&quot;:2}, &quot;720&quot;:{&quot;items&quot;:3}, &quot;991&quot;:{&quot;items&quot;:2}, &quot;1140&quot;:{&quot;items&quot;:3}, &quot;1300&quot;:{&quot;items&quot;:4}, &quot;1500&quot;:{&quot;items&quot;:5}}}">
-								<!-- Product-->
-								<div>
-									<div class="card product-card card-static pb-3">
-										<button class="btn-wishlist btn-sm" type="button" data-toggle="tooltip" data-placement="left" title="Add to wishlist"><i class="czi-heart"></i></button><a class="card-img-top d-block overflow-hidden" href="grocery-single.html"><img src="/images/products/08.jpg" alt="Product"></a>
-										<div class="card-body py-2">
-											<a class="product-meta d-block font-size-xs pb-1" href="#">Dairy and Eggs</a>
-											<h3 class="product-title font-size-sm"><a href="grocery-single.html">Mozzarella Cheese (125g)</a></h3>
-											<div class="product-price"><span class="text-accent">$4.<small>30</small></span></div>
-										</div>
-										<div class="product-floating-btn">
-											<button class="btn btn-primary btn-shadow btn-sm" type="button" data-toggle="toast" data-target="#cart-toast">+<i class="czi-cart font-size-base ml-1"></i></button>
-										</div>
-									</div>
-								</div>
-								<!-- Product-->
-								<div>
-									<div class="card product-card card-static pb-3">
-										<button class="btn-wishlist btn-sm" type="button" data-toggle="tooltip" data-placement="left" title="Add to wishlist"><i class="czi-heart"></i></button><a class="card-img-top d-block overflow-hidden" href="grocery-single.html"><img src="/images/products/09.jpg" alt="Product"></a>
-										<div class="card-body py-2">
-											<a class="product-meta d-block font-size-xs pb-1" href="#">Personal hygiene</a>
-											<h3 class="product-title font-size-sm"><a href="grocery-single.html">Men’s Shampoo (400ml)</a></h3>
-											<div class="product-price"><span class="text-accent">$5.<small>99</small></span></div>
-										</div>
-										<div class="product-floating-btn">
-											<button class="btn btn-primary btn-shadow btn-sm" type="button" data-toggle="toast" data-target="#cart-toast">+<i class="czi-cart font-size-base ml-1"></i></button>
-										</div>
-									</div>
-								</div>
-								<!-- Product-->
-								<div>
-									<div class="card product-card card-static pb-3">
-										<button class="btn-wishlist btn-sm" type="button" data-toggle="tooltip" data-placement="left" title="Add to wishlist"><i class="czi-heart"></i></button><a class="card-img-top d-block overflow-hidden" href="grocery-single.html"><img src="/images/products/10.jpg" alt="Product"></a>
-										<div class="card-body py-2">
-											<a class="product-meta d-block font-size-xs pb-1" href="#">Meat and Poultry</a>
-											<h3 class="product-title font-size-sm"><a href="grocery-single.html">Frozen Oven-ready Poultry</a></h3>
-											<div class="product-price"><span class="text-accent">$12.<small>00</small></span></div>
-										</div>
-										<div class="product-floating-btn">
-											<button class="btn btn-primary btn-shadow btn-sm" type="button" data-toggle="toast" data-target="#cart-toast">+<i class="czi-cart font-size-base ml-1"></i></button>
-										</div>
-									</div>
-								</div>
-								<!-- Product-->
-								<div>
-									<div class="card product-card card-static pb-3">
-										<button class="btn-wishlist btn-sm" type="button" data-toggle="tooltip" data-placement="left" title="Add to wishlist"><i class="czi-heart"></i></button><a class="card-img-top d-block overflow-hidden" href="grocery-single.html"><img src="/images/products/11.jpg" alt="Product"></a>
-										<div class="card-body py-2">
-											<a class="product-meta d-block font-size-xs pb-1" href="#">Snacks, Sweets and Chips</a>
-											<h3 class="product-title font-size-sm"><a href="grocery-single.html">Dark Chocolate with Nuts</a></h3>
-											<div class="product-price"><span class="text-accent">$2.<small>50</small></span></div>
-										</div>
-										<div class="product-floating-btn">
-											<button class="btn btn-primary btn-shadow btn-sm" type="button" data-toggle="toast" data-target="#cart-toast">+<i class="czi-cart font-size-base ml-1"></i></button>
-										</div>
-									</div>
-								</div>
-								<!-- Product-->
-								<div>
-									<div class="card product-card card-static pb-3">
-										<button class="btn-wishlist btn-sm" type="button" data-toggle="tooltip" data-placement="left" title="Add to wishlist"><i class="czi-heart"></i></button><a class="card-img-top d-block overflow-hidden" href="grocery-single.html"><img src="/images/products/12.jpg" alt="Product"></a>
-										<div class="card-body py-2">
-											<a class="product-meta d-block font-size-xs pb-1" href="#">Canned Food and Oil</a>
-											<h3 class="product-title font-size-sm"><a href="grocery-single.html">Corn Oil Bottle (500ml)</a></h3>
-											<div class="product-price"><span class="text-accent">$3.<small>10</small></span></div>
-										</div>
-										<div class="product-floating-btn">
-											<button class="btn btn-primary btn-shadow btn-sm" type="button" data-toggle="toast" data-target="#cart-toast">+<i class="czi-cart font-size-base ml-1"></i></button>
-										</div>
-									</div>
-								</div>
-								<!-- Product-->
-								<div>
-									<div class="card product-card card-static pb-3">
-										<button class="btn-wishlist btn-sm" type="button" data-toggle="tooltip" data-placement="left" title="Add to wishlist"><i class="czi-heart"></i></button><a class="card-img-top d-block overflow-hidden" href="grocery-single.html"><img src="/images/products/13.jpg" alt="Product"></a>
-										<div class="card-body py-2">
-											<a class="product-meta d-block font-size-xs pb-1" href="#">Fish and Seafood</a>
-											<h3 class="product-title font-size-sm"><a href="grocery-single.html">Steak Salmon Fillet (1kg)</a></h3>
-											<div class="product-price"><span class="text-accent">$17.<small>99</small></span></div>
-										</div>
-										<div class="product-floating-btn">
-											<button class="btn btn-primary btn-shadow btn-sm" type="button" data-toggle="toast" data-target="#cart-toast">+<i class="czi-cart font-size-base ml-1"></i></button>
-										</div>
-									</div>
-								</div>
-								<!-- Product-->
-								<div>
-									<div class="card product-card card-static pb-3">
-										<button class="btn-wishlist btn-sm" type="button" data-toggle="tooltip" data-placement="left" title="Add to wishlist"><i class="czi-heart"></i></button><a class="card-img-top d-block overflow-hidden" href="grocery-single.html"><img src="/images/products/14.jpg" alt="Product"></a>
-										<div class="card-body py-2">
-											<a class="product-meta d-block font-size-xs pb-1" href="#">Canned Food and Oil</a>
-											<h3 class="product-title font-size-sm"><a href="grocery-single.html">Sardine in Tomato Sauce (105g)</a></h3>
-											<div class="product-price"><span class="text-accent">$3.<small>25</small></span></div>
-										</div>
-										<div class="product-floating-btn">
-											<button class="btn btn-primary btn-shadow btn-sm" type="button" data-toggle="toast" data-target="#cart-toast">+<i class="czi-cart font-size-base ml-1"></i></button>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</section>
+	<!-- Heading-->
+	<div class="d-flex flex-wrap justify-content-between align-items-center pt-1 border-bottom pb-4 mb-4">
+		<h2 class="h3 mb-0 pt-3 mr-3">Bestsellers</h2>
+		<div class="pt-3"><a class="btn btn-outline-accent btn-sm" href="#">More products<i class="czi-arrow-right ml-1 mr-n1"></i></a></div>
+	</div>
+	<div class="cz-carousel cz-controls-static cz-controls-outside cz-dots-enabled pt-2">
+		<div class="cz-carousel-inner" data-carousel-options="{&quot;items&quot;: 2, &quot;gutter&quot;: 16, &quot;controls&quot;: true, &quot;autoHeight&quot;: true, &quot;responsive&quot;: {&quot;0&quot;:{&quot;items&quot;:1}, &quot;480&quot;:{&quot;items&quot;:2}, &quot;720&quot;:{&quot;items&quot;:3}, &quot;991&quot;:{&quot;items&quot;:2}, &quot;1140&quot;:{&quot;items&quot;:3}, &quot;1300&quot;:{&quot;items&quot;:4}, &quot;1500&quot;:{&quot;items&quot;:5}}}">
+			<!-- Product-->
+			<div>
+				<div class="card product-card card-static pb-3">
+					<button class="btn-wishlist btn-sm" type="button" data-toggle="tooltip" data-placement="left" title="Add to wishlist"><i class="czi-heart"></i></button><a class="card-img-top d-block overflow-hidden" href="grocery-single.html"><img src="/images/products/08.jpg" alt="Product"></a>
+					<div class="card-body py-2">
+						<a class="product-meta d-block font-size-xs pb-1" href="#">Dairy and Eggs</a>
+						<h3 class="product-title font-size-sm"><a href="{{ route('product.show') }}">Mozzarella Cheese (125g)</a></h3>
+						<div class="product-price"><span class="text-accent">$4.<small>30</small></span></div>
+					</div>
+					<div class="product-floating-btn">
+						<button class="btn btn-primary btn-shadow btn-sm" type="button" data-toggle="toast" data-target="#cart-toast">+<i class="czi-cart font-size-base ml-1"></i></button>
+					</div>
+				</div>
+			</div>
+			<!-- Product-->
+			<div>
+				<div class="card product-card card-static pb-3">
+					<button class="btn-wishlist btn-sm" type="button" data-toggle="tooltip" data-placement="left" title="Add to wishlist"><i class="czi-heart"></i></button><a class="card-img-top d-block overflow-hidden" href="grocery-single.html"><img src="/images/products/09.jpg" alt="Product"></a>
+					<div class="card-body py-2">
+						<a class="product-meta d-block font-size-xs pb-1" href="#">Personal hygiene</a>
+						<h3 class="product-title font-size-sm"><a href="{{ route('product.show') }}">Men’s Shampoo (400ml)</a></h3>
+						<div class="product-price"><span class="text-accent">$5.<small>99</small></span></div>
+					</div>
+					<div class="product-floating-btn">
+						<button class="btn btn-primary btn-shadow btn-sm" type="button" data-toggle="toast" data-target="#cart-toast">+<i class="czi-cart font-size-base ml-1"></i></button>
+					</div>
+				</div>
+			</div>
+			<!-- Product-->
+			<div>
+				<div class="card product-card card-static pb-3">
+					<button class="btn-wishlist btn-sm" type="button" data-toggle="tooltip" data-placement="left" title="Add to wishlist"><i class="czi-heart"></i></button><a class="card-img-top d-block overflow-hidden" href="grocery-single.html"><img src="/images/products/10.jpg" alt="Product"></a>
+					<div class="card-body py-2">
+						<a class="product-meta d-block font-size-xs pb-1" href="#">Meat and Poultry</a>
+						<h3 class="product-title font-size-sm"><a href="{{ route('product.show') }}">Frozen Oven-ready Poultry</a></h3>
+						<div class="product-price"><span class="text-accent">$12.<small>00</small></span></div>
+					</div>
+					<div class="product-floating-btn">
+						<button class="btn btn-primary btn-shadow btn-sm" type="button" data-toggle="toast" data-target="#cart-toast">+<i class="czi-cart font-size-base ml-1"></i></button>
+					</div>
+				</div>
+			</div>
+			<!-- Product-->
+			<div>
+				<div class="card product-card card-static pb-3">
+					<button class="btn-wishlist btn-sm" type="button" data-toggle="tooltip" data-placement="left" title="Add to wishlist"><i class="czi-heart"></i></button><a class="card-img-top d-block overflow-hidden" href="grocery-single.html"><img src="/images/products/11.jpg" alt="Product"></a>
+					<div class="card-body py-2">
+						<a class="product-meta d-block font-size-xs pb-1" href="#">Snacks, Sweets and Chips</a>
+						<h3 class="product-title font-size-sm"><a href="{{ route('product.show') }}">Dark Chocolate with Nuts</a></h3>
+						<div class="product-price"><span class="text-accent">$2.<small>50</small></span></div>
+					</div>
+					<div class="product-floating-btn">
+						<button class="btn btn-primary btn-shadow btn-sm" type="button" data-toggle="toast" data-target="#cart-toast">+<i class="czi-cart font-size-base ml-1"></i></button>
+					</div>
+				</div>
+			</div>
+			<!-- Product-->
+			<div>
+				<div class="card product-card card-static pb-3">
+					<button class="btn-wishlist btn-sm" type="button" data-toggle="tooltip" data-placement="left" title="Add to wishlist"><i class="czi-heart"></i></button><a class="card-img-top d-block overflow-hidden" href="grocery-single.html"><img src="/images/products/12.jpg" alt="Product"></a>
+					<div class="card-body py-2">
+						<a class="product-meta d-block font-size-xs pb-1" href="#">Canned Food and Oil</a>
+						<h3 class="product-title font-size-sm"><a href="{{ route('product.show') }}">Corn Oil Bottle (500ml)</a></h3>
+						<div class="product-price"><span class="text-accent">$3.<small>10</small></span></div>
+					</div>
+					<div class="product-floating-btn">
+						<button class="btn btn-primary btn-shadow btn-sm" type="button" data-toggle="toast" data-target="#cart-toast">+<i class="czi-cart font-size-base ml-1"></i></button>
+					</div>
+				</div>
+			</div>
+			<!-- Product-->
+			<div>
+				<div class="card product-card card-static pb-3">
+					<button class="btn-wishlist btn-sm" type="button" data-toggle="tooltip" data-placement="left" title="Add to wishlist"><i class="czi-heart"></i></button><a class="card-img-top d-block overflow-hidden" href="grocery-single.html"><img src="/images/products/13.jpg" alt="Product"></a>
+					<div class="card-body py-2">
+						<a class="product-meta d-block font-size-xs pb-1" href="#">Fish and Seafood</a>
+						<h3 class="product-title font-size-sm"><a href="{{ route('product.show') }}">Steak Salmon Fillet (1kg)</a></h3>
+						<div class="product-price"><span class="text-accent">$17.<small>99</small></span></div>
+					</div>
+					<div class="product-floating-btn">
+						<button class="btn btn-primary btn-shadow btn-sm" type="button" data-toggle="toast" data-target="#cart-toast">+<i class="czi-cart font-size-base ml-1"></i></button>
+					</div>
+				</div>
+			</div>
+			<!-- Product-->
+			<div>
+				<div class="card product-card card-static pb-3">
+					<button class="btn-wishlist btn-sm" type="button" data-toggle="tooltip" data-placement="left" title="Add to wishlist"><i class="czi-heart"></i></button><a class="card-img-top d-block overflow-hidden" href="grocery-single.html"><img src="/images/products/14.jpg" alt="Product"></a>
+					<div class="card-body py-2">
+						<a class="product-meta d-block font-size-xs pb-1" href="#">Canned Food and Oil</a>
+						<h3 class="product-title font-size-sm"><a href="{{ route('product.show') }}">Sardine in Tomato Sauce (105g)</a></h3>
+						<div class="product-price"><span class="text-accent">$3.<small>25</small></span></div>
+					</div>
+					<div class="product-floating-btn">
+						<button class="btn btn-primary btn-shadow btn-sm" type="button" data-toggle="toast" data-target="#cart-toast">+<i class="czi-cart font-size-base ml-1"></i></button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
--- resources/views/home/partials/discount.blade.php
+++ resources/views/home/partials/discount.blade.php
@@ -2,7 +2,7 @@
 	<!-- Heading-->
 	<div class="d-flex flex-wrap justify-content-between align-items-center pt-1 border-bottom pb-4 mb-4">
 		<h2 class="h3 mb-0 pt-3 mr-3">Discounted products</h2>
-		<div class="pt-3"><a class="btn btn-outline-accent btn-sm" href="grocery-catalog.html">More products<i class="czi-arrow-right ml-1 mr-n1"></i></a></div>
+		<div class="pt-3"><a class="btn btn-outline-accent btn-sm" href="#">More products<i class="czi-arrow-right ml-1 mr-n1"></i></a></div>
 	</div>
 	<div class="cz-carousel cz-controls-static cz-controls-outside cz-dots-enabled pt-2">
 		<div class="cz-carousel-inner" data-carousel-options="{&quot;items&quot;: 2, &quot;gutter&quot;: 16, &quot;controls&quot;: true, &quot;autoHeight&quot;: true, &quot;responsive&quot;: {&quot;0&quot;:{&quot;items&quot;:1}, &quot;480&quot;:{&quot;items&quot;:2}, &quot;720&quot;:{&quot;items&quot;:3}, &quot;991&quot;:{&quot;items&quot;:2}, &quot;1140&quot;:{&quot;items&quot;:3}, &quot;1300&quot;:{&quot;items&quot;:4}, &quot;1500&quot;:{&quot;items&quot;:5}}}">
@@ -13,7 +13,7 @@
 					<button class="btn-wishlist btn-sm" type="button" data-toggle="tooltip" data-placement="left" title="Add to wishlist"><i class="czi-heart"></i></button><a class="card-img-top d-block overflow-hidden" href="grocery-single.html"><img src="/images/products/01.jpg" alt="Product"></a>
 					<div class="card-body py-2">
 						<a class="product-meta d-block font-size-xs pb-1" href="#">Fruits and Vegetables</a>
-						<h3 class="product-title font-size-sm"><a href="grocery-single.html">Coconut, Indonesia (piece)</a></h3>
+						<h3 class="product-title font-size-sm"><a href="#">Coconut, Indonesia (piece)</a></h3>
 						<div class="product-price"><span class="text-accent">$1.<small>99</small></span>
 							<del class="font-size-sm text-muted">$2.<small>99</small></del>
 						</div>
@@ -30,7 +30,7 @@
 					<button class="btn-wishlist btn-sm" type="button" data-toggle="tooltip" data-placement="left" title="Add to wishlist"><i class="czi-heart"></i></button><a class="card-img-top d-block overflow-hidden" href="grocery-single.html"><img src="/images/products/02.jpg" alt="Product"></a>
 					<div class="card-body py-2">
 						<a class="product-meta d-block font-size-xs pb-1" href="#">Dairy and Eggs</a>
-						<h3 class="product-title font-size-sm"><a href="grocery-single.html">Soft Creme Cheese (200g)</a></h3>
+						<h3 class="product-title font-size-sm"><a href="{{ route('product.show') }}">Soft Creme Cheese (200g)</a></h3>
 						<div class="product-price"><span class="text-accent">$2.<small>99</small></span>
 							<del class="font-size-sm text-muted">$3.<small>99</small></del>
 						</div>
@@ -47,7 +47,7 @@
 					<button class="btn-wishlist btn-sm" type="button" data-toggle="tooltip" data-placement="left" title="Add to wishlist"><i class="czi-heart"></i></button><a class="card-img-top d-block overflow-hidden" href="grocery-single.html"><img src="/images/products/03.jpg" alt="Product"></a>
 					<div class="card-body py-2">
 						<a class="product-meta d-block font-size-xs pb-1" href="#">Soft Drinks and Juice</a>
-						<h3 class="product-title font-size-sm"><a href="grocery-single.html">Pepsi Soda Can (.33ml)</a></h3>
+						<h3 class="product-title font-size-sm"><a href="{{ route('product.show') }}">Pepsi Soda Can (.33ml)</a></h3>
 						<div class="product-price"><span class="text-accent">$1.<small>00</small></span>
 							<del class="font-size-sm text-muted">$1.<small>25</small></del>
 						</div>
@@ -64,7 +64,7 @@
 					<button class="btn-wishlist btn-sm" type="button" data-toggle="tooltip" data-placement="left" title="Add to wishlist"><i class="czi-heart"></i></button><a class="card-img-top d-block overflow-hidden" href="grocery-single.html"><img src="/images/products/04.jpg" alt="Product"></a>
 					<div class="card-body py-2">
 						<a class="product-meta d-block font-size-xs pb-1" href="#">Fruits and Vegetables</a>
-						<h3 class="product-title font-size-sm"><a href="grocery-single.html">Fresh Orange, Spain (1kg)</a></h3>
+						<h3 class="product-title font-size-sm"><a href="{{ route('product.show') }}">Fresh Orange, Spain (1kg)</a></h3>
 						<div class="product-price"><span class="text-accent">$1.<small>15</small></span>
 							<del class="font-size-sm text-muted">$1.<small>75</small></del>
 						</div>
@@ -81,7 +81,7 @@
 					<button class="btn-wishlist btn-sm" type="button" data-toggle="tooltip" data-placement="left" title="Add to wishlist"><i class="czi-heart"></i></button><a class="card-img-top d-block overflow-hidden" href="grocery-single.html"><img src="/images/products/05.jpg" alt="Product"></a>
 					<div class="card-body py-2">
 						<a class="product-meta d-block font-size-xs pb-1" href="#">Personal hygiene</a>
-						<h3 class="product-title font-size-sm"><a href="grocery-single.html">Moisture Body Lotion (250ml)</a></h3>
+						<h3 class="product-title font-size-sm"><a href="{{ route('product.show') }}">Moisture Body Lotion (250ml)</a></h3>
 						<div class="product-price"><span class="text-accent">$4.<small>20</small></span>
 							<del class="font-size-sm text-muted">$5.<small>99</small></del>
 						</div>
@@ -98,7 +98,7 @@
 					<button class="btn-wishlist btn-sm" type="button" data-toggle="tooltip" data-placement="left" title="Add to wishlist"><i class="czi-heart"></i></button><a class="card-img-top d-block overflow-hidden" href="grocery-single.html"><img src="/images/products/06.jpg" alt="Product"></a>
 					<div class="card-body py-2">
 						<a class="product-meta d-block font-size-xs pb-1" href="#">Snacks, Sweets and Chips</a>
-						<h3 class="product-title font-size-sm"><a href="grocery-single.html">Nut Chocolate Paste (750g)</a></h3>
+						<h3 class="product-title font-size-sm"><a href="{{ route('product.show') }}">Nut Chocolate Paste (750g)</a></h3>
 						<div class="product-price"><span class="text-accent">$6.<small>50</small></span>
 							<del class="font-size-sm text-muted">$7.<small>99</small></del>
 						</div>
@@ -115,7 +115,7 @@
 					<button class="btn-wishlist btn-sm" type="button" data-toggle="tooltip" data-placement="left" title="Add to wishlist"><i class="czi-heart"></i></button><a class="card-img-top d-block overflow-hidden" href="grocery-single.html"><img src="/images/products/07.jpg" alt="Product"></a>
 					<div class="card-body py-2">
 						<a class="product-meta d-block font-size-xs pb-1" href="#">Dairy and Eggs</a>
-						<h3 class="product-title font-size-sm"><a href="grocery-single.html">Mozzarella Mini Cheese</a></h3>
+						<h3 class="product-title font-size-sm"><a href="{{ route('product.show') }}">Mozzarella Mini Cheese</a></h3>
 						<div class="product-price"><span class="text-accent">$3.<small>50</small></span>
 							<del class="font-size-sm text-muted">$4.<small>99</small></del>
 						</div>
