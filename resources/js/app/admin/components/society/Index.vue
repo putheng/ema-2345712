d8b5316d170c4f2f09ab79018bc739080af862de
vagrant <vagrant@homestead>
2020-04-19 11:08:11 +0000

    redesign network

--- resources/js/app/admin/components/society/Index.vue
+++ resources/js/app/admin/components/society/Index.vue
@@ -1,83 +1,206 @@
 <template>
-<div class="section-block">
-	<div class="card card-fluid">
-		<div class="card-body">
-			<h4 class="card-title">
-				Society Member
-				
-				<router-link 
-					:to="{ name:'admin-society-create' }" 
-					class="btn btn-primary float-right">
-					Create
-				</router-link>
-			</h4>
-			
-			<div class="card-text col-md-12">
-				<div class="tree" style="display: flex;">
-				<ul style="margin: auto;">
-					<li v-if="agents">
-						<div class="wrapper">
-							<table>
-								<tbody>
-									<tr>
-										<td >
-										</td>
-										<td>
-											<div class="pl-2">
-												<div class="leading-tight">
-													{{ agents.name }}
-												</div>
-												<div class="text-sm text-bold hidden leading-tight">
-													<span class="text-bold">ID:</span> 
-													{{ agents.uuid }}
-												</div>
-												<div class="text-sm text-bold hidden leading-tight">
-													<span class="text-bold">Memers:</span> 
-													{{ agents.child }}
-												</div>
-											</div>
-										</td>
-									</tr>
-								</tbody>
-							</table>
+<div class="page-inner">
+	<header class="page-title-bar">
+		<div class="page-title">
+			<span>Networks</span>
+			<router-link 
+				:to="{name: 'society-society-create'}"
+				class="btn btn-primary float-right">
+				Create
+			</router-link>
+		</div>
+
+	</header>
+	<div class="page-section">
+		
+		<section class="management-hierarchy">
+		<div class="hv-container">
+			<div class="hv-wrapper">
+				<!-- Key component -->
+				<div class="hv-item">
+					<div class="hv-item-parent">
+						<div class="person">
+							<img :src="agents.avatar" alt="">
+							<p class="name">
+								{{ agents.name }} <b>/ {{ agents.uuid }}</b>
+							</p>
 						</div>
-						<template v-if="agents.children">
-							<Agent :agents="agents"/>
-						</template>
-					</li>
-				</ul>
-			</div>
+					</div>
+					<div class="hv-item-children" v-if="agents.children">
+						<div class="hv-item-child" v-for="(child, k) in agents.children">
+							<!-- Key component -->
+							<div class="hv-item">
+								<div class="hv-item-parent">
+									<div class="person">
+										<img :src="child.avatar" alt="">
+										<p class="name">
+											{{ child.name }} <b>/ {{ child.uuid }}</b>
+										</p>
+									</div>
+								</div>
+								<div class="hv-item-children" v-if="child.children">
+									<div class="hv-item-child" v-for="ch in child.children">
+										<div class="hv-item">
+											<div class="person">
+												<img :src="ch.avatar" alt="">
+												<p class="name">
+													{{ ch.name }} <b>/ {{ ch.uuid }}</b>
+												</p>
+											</div>
+
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
+		</section>
 	</div>
 </div>
 </template>
 
 <script>
-	import { mapGetters } from 'vuex'
+	import { mapGetters, mapActions } from 'vuex'
+
+	import NetworkChildren from './partials/NetworkChildren'
 
 	export default {
 		data(){
 			return {
-				agents: []
+				agents:[]
+			}
+		},
+		components: {
+			NetworkChildren
+		},
+		methods: {
+			...mapActions({
+				fetchNetworks: 'society/fetchNetworks'
+			}),
+			async fetch(){
+				let r = await axios.get(`sociaty/show`)
+
+				this.agents = r.data.data
 			}
 		},
 		computed: {
 			...mapGetters({
-				auth: 'userData'
+				networks: 'society/getNetworks'
 			})
 		},
-
-		methods: {
-			fetchAgent(){
-				axios.get('sociaty/show').then((response) => {
-					this.agents = response.data.data
-				})
-			}
-		},
-
-		mounted(){
-			this.fetchAgent()
+		created(){
+			this.fetch()
 		}
 	}
 </script>
+
+<style lang="scss" scoped>
+@import '@sass/_hierarchy-view.scss';
+@import url('https://fonts.googleapis.com/css?family=Poppins');
+$bottom-margin: 50px;
+$line-width: 2px;
+$line-color: rgba(#FFF, 0.7);
+$bg-color: #EFE6E2;
+html,
+body {
+	height: 100%;
+	font-family: 'Poppins', sans-serif;
+	padding: 0;
+	margin: 0;
+}
+
+section {
+	min-height: 100%;
+	display: flex;
+	justify-content: center;
+	flex-direction: column;
+	padding: 50px 0;
+	position: relative;
+	.github-badge {
+		position: absolute;
+		top: 0;
+		left: 0;
+	}
+	h1 {
+		text-align: center;
+		margin-bottom: 70px;
+	}
+	.hv-container {
+		// display: flex;
+		flex-grow: 1;
+		overflow: auto;
+		// align-items: center;
+		justify-content: center;
+	}
+}
+
+// BASIC STYLE
+.basic-style {
+	// background-color: #EFE6E2;
+	&>h1 {
+		color: #DE5454;
+	}
+}
+
+p.simple-card {
+	margin: 0;
+	background-color: #fff;
+	color: #DE5454;
+	padding: 30px;
+	border-radius: 7px;
+	min-width: 100px;
+	text-align: center;
+	box-shadow: 0 3px 6px rgba(#CC8367, 0.22);
+}
+
+.hv-item-parent {
+	p {
+		font-weight: bold;
+		color: #DE5454;
+	}
+}
+
+// MANAGEMENT HIERARCHY
+.management-hierarchy {
+	&>h1 {
+		color: #FFF;
+	}
+	.person {
+		text-align: center;
+		&>img {
+			height: 70px;
+			border: 2px solid #bbb;
+			border-radius: 50%;
+			overflow: hidden;
+			background-color: #fff;
+		}
+		&>p.name {
+			background-color: #cacaca;
+			padding: 5px 10px;
+			border-radius: 5px;
+			font-size: 12px;
+			font-weight: normal;
+			color: #232322;
+			margin: 0;
+			position: relative;
+			white-space: nowrap;
+			b {
+				color: #ff6500;
+			}
+			&:before {
+				content: '';
+				position: absolute;
+				width: 2px;
+				height: 8px;
+				background-color: #fff;
+				left: 50%;
+				top: 0;
+				transform: translateY(-100%);
+			}
+		}
+	}
+}
+</style>
