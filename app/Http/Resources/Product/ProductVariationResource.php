3f07209b48eb52969643e6ef99d720b26cfb2def
Putheng <puthengemail@gmail.com>
2020-05-30 10:36:29 +0700

    update variation price

--- app/Http/Resources/Product/ProductVariationResource.php
+++ app/Http/Resources/Product/ProductVariationResource.php
@@ -28,6 +28,7 @@ class ProductVariationResource extends JsonResource
             'weight' => $this->weight,
             'pricex' => currency_format($this->price->amount()),
             'tax_price' => currency_format($this->tax_price->amount()),
+            'tax_pricex' => $this->tax_price->amount(),
             'sale_pricex' => currency_format($this->sale_price->amount()),
             'stock' => (int) $this->stockCount(),
             'in_stock' => $this->inStock(),
