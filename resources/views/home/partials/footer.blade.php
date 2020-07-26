9f46c5b7ee7055a89b20fae88bfaf2d77989d311
Putheng <puthengemail@gmail.com>
2020-07-26 10:51:13 +0700

    replace image

--- resources/views/home/partials/footer.blade.php
+++ resources/views/home/partials/footer.blade.php
@@ -113,7 +113,7 @@
 	<div class="bg-darker px-lg-3 py-3">
 		<div class="d-sm-flex justify-content-between align-items-center mx-auto px-3" style="max-width: 80rem;">
 			<div class="font-size-xs text-light opacity-50 text-center text-sm-left py-3">© All rights reserved.</div>
-			<div class="py-3"><img class="d-block mx-auto mx-sm-left" width="187" src="/images/products/cards-alt.png" alt="Payment methods"/>
+			<div class="py-3"><img class="d-block mx-auto mx-sm-left" width="187" src="/images/products/cards-alt.png?v=2" alt="Payment methods"/>
 			</div>
 		</div>
 	</div>
