d7626167b861ecb80cc0fa37b3128a316adf48e2
Putheng <puthengemail@gmail.com>
2020-07-28 09:26:06 +0700

    increse society pagination view

--- app/Http/Controllers/Admin/SocietyController.php
+++ app/Http/Controllers/Admin/SocietyController.php
@@ -13,7 +13,7 @@ class SocietyController extends Controller
     public function index()
     {
     	return SocietyResource::collection(
-    		User::isSociety()->paginate(30)
+    		User::isSociety()->paginate(130)
     	);
     }
 
