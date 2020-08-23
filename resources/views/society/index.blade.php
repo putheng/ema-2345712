86eda49e3a9e7bb637b5bd3fd1b7ab9c89494a8a
Putheng <puthengemail@gmail.com>
2020-08-23 14:16:48 +0700

    change menu style

--- resources/views/society/index.blade.php
+++ resources/views/society/index.blade.php
@@ -10,7 +10,7 @@
 
     	<link rel="shortcut icon" href="/images/logo-s.png">
 		<!-- End FAVICONS -->
-		<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+		<link rel="stylesheet" href="{{ asset('css/app.css?v3') }}">
 		    <!-- CSRF Token -->
 	    <meta name="csrf-token" content="{{ csrf_token() }}">
 	</head>
