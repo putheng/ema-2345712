7fa3b7f1404fb940752b1483152f2a9e3f52c8e5
Putheng <puthengemail@gmail.com>
2020-08-28 11:32:42 +0700

    remove link from app

--- resources/views/app/partials/best-sale.blade.php
+++ resources/views/app/partials/best-sale.blade.php
@@ -10,12 +10,12 @@
 	@foreach($products as $product)
 		<div class="col-6 mb-3">
 			<div class="card product-card card-static pb-3 text-center">
-				<a class="card-img-top d-block overflow-hidden" href="{{ route('app.show', $product) }}">
+				<a class="card-img-top d-block overflow-hidden" href="#">
 					<img style="width: 157px; height: 157px;margin: auto;" src="{{ $product->image->first()->product() }}" alt="Product">
 				</a>
 				<div class="card-body py-2">
 					<h3 class="product-title font-size-sm">
-						<a href="{{ route('app.show', $product) }}">{{ $product->name }}</a>
+						<a href="#">{{ $product->name }}</a>
 					</h3>
 					<div class="product-price">
 		                <span class="text-accent">{{ $product->formattedTaxPrice }}</span>
--- resources/views/product/app.blade.php
+++ resources/views/product/app.blade.php
@@ -30,7 +30,7 @@
       
       @yield('content')
       <!-- Footer-->
-      @include('product.partials.footer')
+      {{-- @include('product.partials.footer') --}}
     </main>
     <!-- Back To Top Button--><a class="btn-scroll-top" href="#top" data-scroll><span class="btn-scroll-top-tooltip text-muted font-size-sm mr-2">Top</span><i class="btn-scroll-top-icon czi-arrow-up">   </i></a>
     </div>
